f56dc8098aea99906895e600219c5137ac6553df
denelkss <kryachkoanast@gmail.com>
2023-12-23 20:37:54 +0300

    add map opening

--- main.py
+++ main.py
@@ -1,19 +1,13 @@
 import pygame
 import pytmx
 
-# Инициализация Pygame
+
 pygame.init()
 
-# Размер окна
 WINDOW_SIZE = (800, 600)
-
-# Создание окна
 screen = pygame.display.set_mode(WINDOW_SIZE)
+tmx_map = pytmx.load_pygame("map.tmx") # будующая карта
 
-# Загрузка карты
-tmx_map = pytmx.load_pygame("maps\carta.tmx")
-
-# Функция отрисовки карт
 def draw_map():
     for layer in tmx_map.visible_layers:
         if isinstance(layer, pytmx.TiledTileLayer):
@@ -22,19 +16,12 @@ def draw_map():
                 screen.blit(tile, (x * tmx_map.tilewidth, y * tmx_map.tileheight))
 
 
-# Главный цикл игры
 running = True
 while running:
-    # Обработка событий Pygame
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    # Отрисовка карты
     draw_map()
-
-    # Обновление экрана
     pygame.display.update()
-
-# Выход из Pygame
 pygame.quit()
