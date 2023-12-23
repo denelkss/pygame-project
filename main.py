553e00fc51a8609f6df20f9c7710adde4e057397
denelkss <kryachkoanast@gmail.com>
2023-12-23 20:33:37 +0300

    add hero

--- main.py
+++ main.py
@@ -1,6 +1,40 @@
-def main():
-    print('aaaaa')
+import pygame
+import pytmx
+
+# Инициализация Pygame
+pygame.init()
+
+# Размер окна
+WINDOW_SIZE = (800, 600)
+
+# Создание окна
+screen = pygame.display.set_mode(WINDOW_SIZE)
+
+# Загрузка карты
+tmx_map = pytmx.load_pygame("maps\carta.tmx")
+
+# Функция отрисовки карт
+def draw_map():
+    for layer in tmx_map.visible_layers:
+        if isinstance(layer, pytmx.TiledTileLayer):
+            for x, y, gid in layer:
+                tile = tmx_map.get_tile_image_by_gid(gid)
+                screen.blit(tile, (x * tmx_map.tilewidth, y * tmx_map.tileheight))
 
 
-if __name__ == '__main__':
-    main()
+# Главный цикл игры
+running = True
+while running:
+    # Обработка событий Pygame
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # Отрисовка карты
+    draw_map()
+
+    # Обновление экрана
+    pygame.display.update()
+
+# Выход из Pygame
+pygame.quit()
