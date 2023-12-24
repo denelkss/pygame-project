d256c0ef9fe52c7252520e7612ac3b732b34ddef
denelkss <kryachkoanast@gmail.com>
2023-12-24 21:08:56 +0300

    add 16 line

--- main.py
+++ main.py
@@ -4,16 +4,17 @@ import pytmx
 
 pygame.init()
 
-WINDOW_SIZE = (800, 600)
+WINDOW_SIZE = (1800, 1000)
 screen = pygame.display.set_mode(WINDOW_SIZE)
-tmx_map = pytmx.load_pygame("map.tmx") # будующая карта
+tmx_map = pytmx.load_pygame("map.tmx") # будущая карта
 
 def draw_map():
     for layer in tmx_map.visible_layers:
         if isinstance(layer, pytmx.TiledTileLayer):
             for x, y, gid in layer:
                 tile = tmx_map.get_tile_image_by_gid(gid)
-                screen.blit(tile, (x * tmx_map.tilewidth, y * tmx_map.tileheight))
+                if tile != None:
+                    screen.blit(tile, (x * tmx_map.tilewidth, y * tmx_map.tileheight))
 
 
 running = True
@@ -21,6 +22,7 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
 
     draw_map()
     pygame.display.update()
