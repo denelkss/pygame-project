567e5afb89e5ab1847e9dbf713cd1de550f3e3ed
denelkss <kryachkoanast@gmail.com>
2023-12-25 23:16:55 +0300

    x

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import pygame
 import pytmx
+import os
 
 
 pygame.init()
+pygame.display.set_caption("Танджиро: в поисках Незуко")
 
-WINDOW_SIZE = (2400, 640)
-screen = pygame.display.set_mode(WINDOW_SIZE)
+screen_size = width_size, height_size = 2400, 640
+screen = pygame.display.set_mode(screen_size)
 tmx_map = pytmx.load_pygame("images\map\map1.tmx")
 
 def draw_map():
