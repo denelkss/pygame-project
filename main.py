b4a0f828c5f98682887e83520910c6561f1dcd65
denelkss <kryachkoanast@gmail.com>
2024-01-10 16:59:17 +0300

    добавление меню в основной код игры

--- main.py
+++ main.py
@@ -1,112 +1,240 @@
 import pygame
 import pytmx
 import os
-from csv import reader
+import sys
 
 
 pygame.init()
 pygame.display.set_caption("Танджиро: в поисках Незуко")
-
 screen_size = width_size, height_size = 1200, 640
 screen = pygame.display.set_mode(screen_size)
-tile_size = 16  # размер одного тайла
-clock = pygame.time.Clock()
+
+background = pygame.image.load("images/menu/background.png")  # фон для меню
+
 map1 = pytmx.load_pygame("images/map/map1.tmx")
+map2 = pytmx.load_pygame("images/map/level2.tmx")
 
-# информация о 1 уровне
-level1 = {'other': 'images/map/map1/map1_other.csv',
-          'earth 2': 'images/map/map1/map1_earth 2.csv',
-          'earth': 'images/map/map1/map1_earth.csv',
-          'rocks': 'images/map/map1/map1_rocks.csv',
-          'sea': 'images/map/map1/map1_sea.csv',
-          'clouds': 'images/map/map1/map1_clouds.csv',
-          'sky': 'images/map/map1/map1_sky.csv'}
+class Button:
+    # text_color - цвет текста, text_color2 - цвет текста при наведении курсора
+    def __init__(self, image, pos, font, text_input, text_color, text_color2):
+        self.image = image
+        self.pos_x, self.pos_y = pos[0], pos[1]
+        self.font = font
+        self.text_input = text_input
+        self.text_color = text_color
+        self.text_color2 = text_color2
+
+        self.text = self.font.render(self.text_input, True, self.text_color)
+        if self.image == None:
+            self.image = self.text
+
+        self.rect = self.image.get_rect(center=(self.pos_x, self.pos_y))
+        self.text_rect = self.text.get_rect(center=(self.pos_x, self.pos_y))
+
+    def update(self, screen):
+        if self.image is not None:
+            screen.blit(self.image, self.rect)
+        screen.blit(self.text, self.text_rect)
+
+    # проверка наведён ли курсор на кнопку
+    def check_input(self, pos):
+        if pos[0] in range(self.rect.left, self.rect.right) and pos[1] in range(self.rect.top, self.rect.bottom):
+            return True
+        else:
+            return False
+
+    # изменение цвета при наведении курсора
+    def change_color(self, pos):
+        if pos[0] in range(self.rect.left, self.rect.right) and pos[1] in range(self.rect.top, self.rect.bottom):
+            self.text = self.font.render(self.text_input, True, self.text_color2)
+        else:
+            self.text = self.font.render(self.text_input, True, self.text_color)
 
 
+# возвращает шрифт в нужном размере
+def get_font(size):
+    return pygame.font.Font("images/menu/font.ttf", size)
 
-# функция для импорта csv файлов
-def csv_layout(path):
-    with open(path) as map:
-        earth_map = []
-        level = reader(map, delimiter=',')
-        for row in level:
-            earth_map.append(list(row))
-        return earth_map
 
-# функция для создания тайлов из png картинки
-def cut_tileset(path):
-    surface = pygame.image.load(path).convert_alpha()
+# играть
+def play():
+    while True:
+        screen.blit(background, (0, 0))
+        play_mouse_pos = pygame.mouse.get_pos()
 
-    # количество тайлов (в длину и высоту)
-    tile_count_x = surface.get_size()[0] // tile_size
-    tile_count_y = surface.get_size()[1] // tile_size
+        back_image = pygame.image.load("images/menu/button_back.png")  # фон кнопки (серый)
+        level1_button = Button(back_image, (600, 150), get_font(60), "уровень 1", (215, 252, 244),
+                             "white")
+        level2_button = Button(back_image, (600, 300), get_font(60), "уровень 2", (215, 252, 244),
+                             "white")
+        level3_button = Button(back_image, (600, 450), get_font(60), "уровень 3", (215, 252, 244),
+                             "white")
 
-    cut_tiles = []  # вырезанные тайлы
-    for row in range(tile_count_y):
-        for col in range(tile_count_x):
-            x = col * tile_size
-            y = row * tile_size
-            new_surface = pygame.Surface((tile_size, tile_size))
-            new_surface.blit(surface, (0, 0), pygame.Rect(x, y, tile_size, tile_size))
-            cut_tiles.append(new_surface)
+        back_play = Button(None, (600, 550), get_font(60), "назад", (105, 105, 105),
+                           "green")
 
-    return cut_tiles
+        for button in[level1_button, level2_button, level3_button, back_play]:
+            button.change_color(play_mouse_pos)
+            button.update(screen)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if back_play.check_input(play_mouse_pos):
+                    main_menu()
+                if level1_button.check_input(play_mouse_pos):
+                    tiles = draw_map(map1)
+                    tiles.draw(screen)
+                if level2_button.check_input(play_mouse_pos):
+                    pass
+                if level3_button.check_input(play_mouse_pos):
+                    pass
+
+        pygame.display.update()
+
+
+# правила игры
+def rules_game():
+    while True:
+        screen.blit(background, (0, 0))
+
+        rules_mouse_pos = pygame.mouse.get_pos()
+
+        back_rules = Button(None, (600, 550), get_font(60), "назад", (105, 105, 105),
+                            "green")
+
+        back_rules.change_color(rules_mouse_pos)
+        back_rules.update(screen)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if back_rules.check_input(rules_mouse_pos):
+                    main_menu()
+
+        pygame.display.update()
+
+# главное меню
+def main_menu():
+    while True:
+        screen.blit(background, (0, 0))
+
+        menu_mouse_pos = pygame.mouse.get_pos()
+
+        text_menu = get_font(100).render("Главное Меню", True, (105, 105, 105))
+        menu_rect = text_menu.get_rect(center=(600, 100))
+
+        # фон кнопки (серый)
+        back_image = pygame.image.load("images/menu/button_back.png")
+
+        play_button = Button(back_image, (600, 250), get_font(70), "играть",  (215, 252, 244),
+                             "white")
+        rules_button = Button(back_image, (600, 400), get_font(70), "правила",
+                              (215, 252, 244), "white")
+        quit_button = Button(back_image, (600, 550), get_font(70), "выйти", (215, 252, 244),
+                             "white")
+
+        screen.blit(text_menu, menu_rect)
+
+        for button in [play_button, rules_button, quit_button]:
+            button.change_color(menu_mouse_pos)
+            button.update(screen)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if play_button.check_input(menu_mouse_pos):
+                    play()
+                if rules_button.check_input(menu_mouse_pos):
+                    rules_game()
+                if quit_button.check_input(menu_mouse_pos):
+                    pygame.quit()
+                    sys.exit()
+
+        pygame.display.update()
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        super().__init__()
+        self.x = x
+        self.y = y
+        self.speed = 20
+        self.current_frame = 0
+        self.frames = []
+        self.load_frames()
+        self.image = self.frames[0]
+        self.rect = self.image.get_rect()
+        self.rect.center = (self.x, self.y)
+
+    def load_frames(self):
+        spritesheet = pygame.image.load("images/sprites/hero/hero_run.png").convert_alpha()
+        for i in range(10):
+            self.frames.append(spritesheet.subsurface((i * 32, 0, 80, 80)))
+
+    def update(self, keys):
+        if keys[pygame.K_LEFT]:
+            self.x -= self.speed
+        if keys[pygame.K_RIGHT]:
+            self.x += self.speed
+        if keys[pygame.K_UP]:
+            self.y -= self.speed
+        if keys[pygame.K_DOWN]:
+            self.y += self.speed
+
+        self.current_frame += 1
+        if self.current_frame >= len(self.frames):
+            self.current_frame = 0
+        self.image = self.frames[self.current_frame]
+        self.rect.center = (self.x, self.y)
+
 
 class Tile(pygame.sprite.Sprite):
-	def __init__(self, size, x, y):
-		super().__init__()
-		self.image = pygame.Surface((size, size))
-		self.rect = self.image.get_rect(topleft = (x,y))
-
-	def update(self,shift):
-		self.rect.x += shift
-
-class StaticTile(Tile):
-	def __init__(self, size, x, y, surface):
-		super().__init__(size, x, y)
-		self.image = surface
-
-# класс уровня
-class Level:
-    def __init__(self, data_level, surface):
-        self.display_surface = surface
-
-        earth_layout = csv_layout(level1['earth'])
-        self.earth_sprites = self.create_tileset(earth_layout, 'earth')
-
-    def create_tileset(self, layoyt, name_tile):
-        sprite_group = pygame.sprite.Group()
-
-        for index_row, row in enumerate(layoyt):
-            for index_col, value in enumerate(row):  # найти индекс и значение x
-                if value != '-1':  # -1, если пусто
-                    x = index_col * tile_size
-                    y = index_row * tile_size
-
-                    if name_tile == 'earth':
-                        earth_tile = cut_tileset('images/map/tilesets_png/tileset1.png')
-                        tile_surface = earth_tile[int(value)]
-                        sprite = StaticTile(tile_size, x, y, tile_surface)
-                    sprite_group.add(sprite)
-
-        return sprite_group
-
-    def run(self):
-        self.earth_sprites.draw(self.display_surface)
+    def __init__(self, image, x, y):
+        super().__init__()
+        self.image = image
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
 
 
-level = Level(level1, screen)
+def draw_map(tmx_map):
+    tiles = pygame.sprite.Group()
+    for layer in tmx_map.visible_layers:
+        if isinstance(layer, pytmx.TiledTileLayer):
+            for x, y, gid in layer:
+                tile_image = tmx_map.get_tile_image_by_gid(gid)
+                if tile_image != None:
+                    tile = Tile(tile_image, x * tmx_map.tilewidth, y * tmx_map.tileheight)
+                    tiles.add(tile)
+                    screen.blit(tile_image, (x * tmx_map.tilewidth, y * tmx_map.tileheight))
+    return tiles
+
+
+main_menu()
+player = Player(100, 100)
 
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    screen.fill('black')
-    level.run()
 
-    pygame.display.update()
-    clock.tick(60)
+    keys = pygame.key.get_pressed()
+
+    #tiles = draw_map(map1)
+
+    player.update(keys)
+
+    screen.fill((0, 0, 0))
+    #tiles.draw(screen)
+    screen.blit(player.image, player.rect)
+    pygame.display.flip()
+
 pygame.quit()
-
-# 1 уровень - 2400 x 640 (150 х 40 тайлов, 16 х 16 - 1 тайл)
