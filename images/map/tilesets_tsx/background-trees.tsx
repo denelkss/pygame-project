92bc2fa5780a4e75462d80c36f77f87a66e16ea3
denelkss <kryachkoanast@gmail.com>
2024-01-10 21:09:42 +0300

    картинки

--- images/map/tilesets_tsx/background-trees.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="background-trees" tilewidth="16" tileheight="16" tilecount="896" columns="56">
- <image source="../tilesets_png/background-trees.png" width="896" height="256"/>
-</tileset>
